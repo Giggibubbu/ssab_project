8a0cd8b538da47b0a23273db5525bab38c08bbe5
Giggibubbu <you@example.com>
2023-05-03 23:02:44 +0200

    - Updated management.sol

--- sc_mngmt/management.sol
+++ sc_mngmt/management.sol
@@ -3,7 +3,15 @@ pragma solidity >0.7.17;
 
 contract BlockchainLoadBalancing {
 
+    uint256 internal numberOfShard;
+    uint256 internal maxDeployedContracts;
+    uint256 internal counter;
+    uint256 internal targetShardForDeploy;
+    mapping(address => uint256) contractToShard;
+
+    Shard[] blockchain;
     enum CounterInternalStates {CounterChanged, CounterNotChanged}
+    CounterInternalStates internal counterState;
     
     struct SmartContract
     {
@@ -13,27 +21,28 @@ contract BlockchainLoadBalancing {
     }
     
     struct Shard {
-        string httpsAddress;
+        string httpAddress;
         uint256 bchNumber;
         address[] contractsArray;
         mapping(address => SmartContract) smartContracts;
     }
 
+    event Deploy(
+        address contractAddress,
+        string message,
+        uint256 deployedToShard
+    );
 
-    Shard[] blockchain;
-
-    uint256 internal numberOfShard;
-    uint256 internal maxDeployedContracts;
-    uint256 internal counter;
-    CounterInternalStates internal counterState;
-    uint256 internal targetShardForDeploy;
-    mapping(address => uint256) contractToShard;
-
+    event ShardState(
+        address sender,
+        uint256 whereToDeploy
+    );
 
     // Initializes Shards and internal variables
-    constructor(uint256 shardCount, string[] memory httpsAddress)
+    constructor(uint256 shardCount, string[] memory httpAddress)
     {
-        require(shardCount == httpsAddress.length, "shardCount must be the same length as httpsAddress array");
+        require(shardCount == httpAddress.length,
+        "shardCount must be the same length as httpsAddress array");
         
         numberOfShard = shardCount;
         counter = 0;
@@ -42,19 +51,21 @@ contract BlockchainLoadBalancing {
         for (uint256 i = 0; i<shardCount; i++)
         {
             blockchain.push();
-            blockchain[i].httpsAddress = httpsAddress[i];
+            blockchain[i].httpAddress = httpAddress[i];
             blockchain[i].bchNumber = i;
         }
-     
     }
 
-    /*  Communicates to OffchainManager the index of the shard where the deploy has to be executed.
+    /*  Communicates to OffchainManager the index of the shard where
+        the deploy has to be executed.
         
-        Ex. If there are four shard, the first deploy will be executed on shard n. 0. Then, 1, 2, 3.
-        When the counter is equal to four, it will be resetted to 0.
+        Ex. If there are four shard, the first deploy will be executed on shard n. 0.
+        Then, 1, 2, 3. When the counter is equal to four, it will be resetted to 0.
 
-        If there's no confirmDeploy after the previous execution of whereToDeploy, reverts changes of the counter.*/
-    function whereToDeploy() public returns (uint256)
+        If there's no confirmDeploy after the previous execution of whereToDeploy,
+        reverts changes of the counter.
+    */
+    function whereToDeploy() public
     {
         revertCounterChanges();
         if(counter<numberOfShard)
@@ -69,11 +80,13 @@ contract BlockchainLoadBalancing {
             counter++;
         }
         counterState = CounterInternalStates.CounterChanged;
-        return targetShardForDeploy;
+        emit ShardState(msg.sender, targetShardForDeploy);
     }
 
     /* Called after the successful deploy by the OffchainManager.
-       Writes smart contract data inside the corresponding target shard struct of the OnChainManager. */
+       Writes smart contract data inside the corresponding target shard struct of
+       the OnChainManager.
+    */
     function confirmDeploy(address contractAddress, string calldata contractName, string calldata contractBinary) public
     {
 
@@ -82,27 +95,32 @@ contract BlockchainLoadBalancing {
         blockchain[targetShardForDeploy].smartContracts[contractAddress].owner = msg.sender;
         blockchain[targetShardForDeploy].contractsArray.push(contractAddress);
         contractToShard[contractAddress] = targetShardForDeploy;
-        counterState = CounterInternalStates.CounterNotChanged;  
+        counterState = CounterInternalStates.CounterNotChanged;
+        emit Deploy(contractAddress, "deploy confirmed to shard n. ", targetShardForDeploy);  
     }
 
     /* Resets counter when whereToDeploy was called but no confirmDeploy were executed. */
     function revertCounterChanges() private 
     {
-        if(counterState == CounterInternalStates.CounterChanged)
+        if(counterState != CounterInternalStates.CounterNotChanged)
         {
             counter--;
         }
     }
 
-    function whereIsContractDeployed(address contractAddress) public view returns (string memory, string memory, uint256)
+    // Returns  the shard where the contract with address contractAddress is deployed
+    function whereIsContractDeployed(address contractAddress) public view returns (uint256)
     {
-        SmartContract memory smartContract = blockchain[contractToShard[contractAddress]].smartContracts[contractAddress];
-        return (smartContract.contractName, smartContract.contractBinary, contractToShard[contractAddress]);
+        return (contractToShard[contractAddress]);
     }
 
-    function printContract(address contractName) public view
+    function returnShardHttpAddresses() public view returns (string[] memory httpAddress)
     {
-        
+        for(uint256 i = 0; i<numberOfShard; i++)
+        {
+            httpAddress[i] = blockchain[i].httpAddress;
+        }
+        return httpAddress;
     }
 
 
